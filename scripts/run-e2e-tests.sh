1e2f509bfb65932b6f2ca4654cdbfe93172624f8
suswari <suswari91@gmail.com>
2018-06-15 13:16:47 -0400

    test

--- scripts/run-e2e-tests.sh
+++ scripts/run-e2e-tests.sh
@@ -14,8 +14,8 @@ npm run tsc
 # Run the Selenium installation script, located in the local node_modules/ directory.
 # This script downloads the files required to run Selenium itself and build a start script and a directory with them.
 # When this script is finished, we can start the standalone version of Selenium with the Chrome driver by executing the start script.
-# node ./node_modules/protractor/bin/webdriver-manager update --versions.chrome=2.38 --versions.standalone=2.3.1
-sudo webdriver-manager update --versions.chrome=2.38 --versions.standalone=3.3.1
+node ./node_modules/protractor/bin/webdriver-manager update
+# sudo webdriver-manager update --versions.chrome=2.38 --versions.standalone=3.3.1
 # npm run webdriver-update
 # X11 for Ubuntu is not configured! The following configurations are needed for XVFB.
 # Make a new display :21 with virtual screen 0 with resolution 1024x768 24dpi
@@ -25,9 +25,9 @@ Xvfb :10 -screen 0 1920x1080x24 2>&1 >/dev/null &
 
 # Right now this is not necessary, because of 'directConnect: true' in the 'e2e.conf.js'
 echo "Starting webdriver"
-webdriver-manager start --versions.chrome=2.38 --versions.standalone=3.3.1 > /dev/null 2>&1 &
+# webdriver-manager start --versions.chrome=2.38 --versions.standalone=3.3.1 > /dev/null 2>&1 &
 # sudo webdriver-manager start --versions.chrome=2.38 > /dev/null 2>&1 &
-# node ./node_modules/protractor/bin/webdriver-manager start [OR webdriver-manager start] &
+node ./node_modules/protractor/bin/webdriver-manager start [OR webdriver-manager start] &
 echo "Finished starting webdriver"
 sleep 10
 
